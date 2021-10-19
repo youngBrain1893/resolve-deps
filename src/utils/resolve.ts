a05ed10662bdc5876bbe6252b32ef39a5254be7c
youngBrain1893 <youngBrain1893@gmail.com>
2021-10-19 20:56:07 +0800

    fix: fix satisifiedExtension check error

--- src/utils/resolve.ts
+++ src/utils/resolve.ts
@@ -1,22 +1,13 @@
 import path from 'path'
 
-export const satisfiedExtensions = (
-  filename: string,
-  extensions: string[]
+export const validateImportPath = (
+  importPath: string,
+  validConfig: { extensions: string[] }
 ): boolean => {
-  let mayFileName = filename.replace(/^\.+/, '')
-  mayFileName = mayFileName.split(path.sep).pop()!
+  const extname = path.extname(importPath);
+  if (!extname) return true;
 
-  if (!mayFileName || mayFileName.split('.').length < 2) {
-    return true
-  }
-
-  const extensionRegs = extensions.map((extension) => {
-    const regText = extension.replace(/^\./, '^\\.')
-    return new RegExp(regText)
-  })
-
-  return extensionRegs.some((reg) => mayFileName.match(reg))
+  return validConfig?.extensions?.includes(extname);
 }
 
 export const getMayPkgName = (importPath: string): string => {
